78737bcea4cbc11edafd6754a0bfc712f1fd451e
Your Name <you@example.com>
2017-11-02 20:52:00 -0700

    added html for homepage

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,5 @@
+//route to POST "/api/friends" that handles all incoming survey results and compatability logi
+
+
+
+//a POST route "/api/friends" that handles all incoming survey results and also handles the compatability logic
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,7 @@
+//route to GET "/survey" that displays the survey page
+
+
+//a "home.html" that displays the home page
+
+
+
