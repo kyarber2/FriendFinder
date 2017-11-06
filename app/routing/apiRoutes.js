1b3a8adbf839567c08f084973344d037ecc9a78c
Your Name <you@example.com>
2017-11-05 20:25:33 -0800

    updated html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,38 @@
-//route to POST "/api/friends" that handles all incoming survey results and compatability logi
+//route to POST "/api/friends" that handles all incoming survey results and compatability logic
+var express = require('express'),
+    fs = require('fs')
+    url = require('url');
+var app = express();
+var friendData = require('../data/friends.js')
+
+
+app.use('/data', express.static(__dirname + '/data'));  
+app.use(express.static(__dirname + '/data')); 
+
+app.post('/api/friends', function(request, respond) {
+	friendData.push(req.body);
+	res.json();
+    // var body = '';
+    // filePath = __dirname + '/data/friends.js';
+    // request.on('data', function(data) {
+    //     body += data;
+    // });
+
+    // request.on('end', function (){
+    //     fs.appendFile(filePath, body, function() {
+    //         respond.end();
+    //     });
+    // });
+});
+
+app.listen(3000);
 
 
 
-//a POST route "/api/friends" that handles all incoming survey results and also handles the compatability logic
+
+
+//A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+//put name of array into the res.json()
+app.get("/api/friends", function(req, res) {
+    res.json("/friends.js");
+  });
--- server.js
+++ server.js
@@ -4,3 +4,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var port = 3000;
+var apiRoutes = require("./app/routing/apiRoutes.js");
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
